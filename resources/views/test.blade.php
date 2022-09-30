a70662833512d4e8f3f0301855c7b47c143fbbb1
sergey-geomotiv <sergey.k@geomotiv.com>
2022-09-30 17:17:44 +0300

    project creation

---
+++ resources/views/test.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>test </title>
+
+
+    </head>
+    <body class="antialiased">
+            <div>
+                test
+            </div>
+    </body>>
+</html>
